82ed43383f26a38931f6b793656c8e16fa4fabe2
anosov34 <bookmake339@gmail.com>
2021-09-19 10:35:34 +0300

    поправил согласно твои комментам)

--- src/pro/sky/java/course1/lesson5/Homework5Massive.java
+++ src/pro/sky/java/course1/lesson5/Homework5Massive.java
@@ -1,61 +1,71 @@
 package pro.sky.java.course1.lesson5;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class Homework5Massive {
-    public static void main(String[] args) {
-        int[] task1 = generateRandomArray();
-        int[] task2 = generateRandomArray2();
-        int[] task3 = generateRandomArray3();
-        task4();
-        task5();
-    }
-    // task1
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
-        int sum = 0;
-        for (int index = 0; index < arr.length; index++) {
-            arr[index] = random.nextInt(100_000) + 100_000;
-            sum = sum + arr[index];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
         }
-        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
         return arr;
     }
-// task2
-    public static int[] generateRandomArray2() {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+    }
+
+    public static void task1 () {
+        Random random = new Random();
+        int[] expenses = generateRandomArray();
+        int sum = 0;
+        for (int daily = 0; daily < expenses.length; daily++) {
+            expenses[daily] = random.nextInt(100_000) + 100_000;
+            sum += expenses[daily];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+
+    }
+
+    public static void task2() {
         java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
-        int  maxArr = -1;
-        int minArr = 201_000;
+        int[] arr = generateRandomArray();
+        int maxArr = arr[0];
+        int minArr = arr[0];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
             if (arr[i] < minArr) {
-                minArr = arr [i];
+                minArr = arr[i];
             }
         }
         for (int index = 0; index < arr.length; index++) {
             arr[index] = random.nextInt(100_000) + 100_000;
             if (arr[index] > maxArr) {
-                maxArr = arr [index];
+                maxArr = arr[index];
             }
         }
-        System.out.println( "Максимальная сумма трат за месяц составила " +  maxArr +  " рублей. Минимальная сумма трат за месяц составила " + minArr + " рублей.");
-            return arr;
+        System.out.println("Максимальная сумма трат за месяц составила " + maxArr + " рублей. Минимальная сумма трат за месяц составила " + minArr + " рублей.");
+
     }
-//    task3
-    public static int[] generateRandomArray3() {
+
+    public static void task3() {
         java.util.Random random = new java.util.Random();
-        int[] arr = new int[30];
+        int[] arr = generateRandomArray();
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
-            sum = sum + arr[i];
+            sum += arr[i];
         }
-        System.out.println("Средняя сумма трат за месяц составила " +  sum / arr.length + " рублей");
-        return arr;
-//        Знаю, что в этой задаче нужно было сделать с дробными, но не смог понять как=(
+        double summary = sum;
+        double averageExpenses = summary / arr.length;
+        System.out.println("Средняя сумма трат за месяц составила " +  averageExpenses + " рублей");
     }
+
     private static void task4() {
         char[] reverseFullName = { 'n','a','v','I',' ','v','o','n','a','v','I'};
         for (int i = reverseFullName.length - 1; i >= 0; i--) {
